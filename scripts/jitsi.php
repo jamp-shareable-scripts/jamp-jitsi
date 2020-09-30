96abe0ece37e7797c6cbacfaa5170d6f47a7e07b
Will <756769+wclear@users.noreply.github.com>
2020-09-30 18:55:25 +0200

    Use default if non-numeric

--- scripts/jitsi.php
+++ scripts/jitsi.php
@@ -20,10 +20,10 @@ $defaultLength = 16;
 $lengthRaw = empty($opts['length'])
 ? (empty($opts['l']) ? $defaultLength : $opts['l'])
 : $opts['length'];
-$length = is_numeric($lengthRaw) ? (int)$lengthRaw : 64;
+$length = is_numeric($lengthRaw) ? (int)$lengthRaw : $defaultLength;
 
 $random_string = trim(exec(
-	'jamp random-string --letters --numbers --length=' . $length;
+	'jamp random-string --letters --numbers --length=' . $length
 ));
 $link = "https://meet.jit.si/" . $random_string;
 jampEcho($link);
